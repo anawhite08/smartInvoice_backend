24d3caab6092d66e928a7d9120f8a0785ea09686
Ana Blanco <ana.blanco@abside.com>
2026-07-17 15:26:48 -0400

    Update

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ flask_app = create_app()
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    flask_app.run(host="0.0.0.0", port=port)
+    flask_app.run(host="0.0.0.0", port=port, debug=True)
